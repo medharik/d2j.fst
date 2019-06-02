ee977f9753078f240c338dedc39b6372cbab053f
unknown <med.harik@gmail.com>
2019-06-02 13:40:36 +0000

    tp 2 php + ngrok + affectation ternaire

--- tp1.php
+++ tp1.php
@@ -1,5 +1,42 @@
 <?php 
-$nom ="mohamed";
-echo "salut $nom";
+//variables :
+$nom ='D2J';
+ echo $nom."<br>"   ;
+ $b=10;
+ $age=19;
+ $a=$age ;// 19 passage par valeur (by val)
+$b=&$age;//passage par adresse par ref (by ref)
+ $age =28;
+ echo "<h2>b est $b</h2>";
+ echo "a est ".$a."<br>";
+ echo $age."ans<br>"   ;
+echo "<hr>";
+echo "<p>age est $age ans</p>";
+echo '<p>age est $age ans</p>';
+echo '<p>age est '. $age.' ans</p>';
+echo getType($b);
+//var_dump($b)
+
+// equivelence boolean;
+//false <=> 0,0.0, "" , '' , "0", null, array()
+function bl($v){
+return (bool) $v;
+}
+
+echo "<hr>";
+var_dump(bl(null));
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>tp 1</title>
+</head>
+<body>
+
+    
+</body>
+</html>
---
+++ tp2.php
@@ -0,0 +1,31 @@
+<?php 
+$name ="Ali";
+$age =26;
+$gender='male';
+// if($age>18){
+// $m="Majeur";
+// }else{
+// $m="Mineur";
+
+// }
+$m=($age>21)? "majeur":"mineur";
+$color=($gender=="male")? "skyblue":"pink";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tp2</title>
+</head>
+<body style="background:<?=$color  ?>" >
+<h2  >Bienvenue <?php echo  $name;?>, votre age est de <?=$age?> ans, vous etes <?=$m?> </h2>
+<?php 
+echo "<h2 >bienvenue $name</h2>";
+
+?>
+    
+</body>
+</html>
